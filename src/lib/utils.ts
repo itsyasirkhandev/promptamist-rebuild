6256490be9af7aa2ec346cd1c450974da836c7e5
yasir <itsyasirkhan.dev@gmail.com>
2026-05-06 14:20:32 +0500

    remvoe un used deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { formatDistanceToNow } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function formatRelativeTime(date: number | string | Date) {
-  return formatDistanceToNow(new Date(date), { addSuffix: true });
 }
